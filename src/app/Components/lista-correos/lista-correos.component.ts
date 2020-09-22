9df510ea7eafade8dfc2df6b1e2e9fd988c1739b
Jose <email>
2020-09-22 16:34:32 +0200

    Ya cargamos los correos cuando tenemos la session inicia y ademas los…
    … correos son visibles y no son sus identificadores.

--- src/app/Components/lista-correos/lista-correos.component.ts
+++ src/app/Components/lista-correos/lista-correos.component.ts
@@ -18,7 +18,7 @@ export class ListaCorreosComponent implements OnInit {
   }
 
   ngOnInit(){
-    console.log(this.getRecibidos());
+    this.getRecibidos();
   }
 
   clickResponder(correo){
@@ -34,9 +34,31 @@ export class ListaCorreosComponent implements OnInit {
     this.gmail.getRecibidos().subscribe(
       (response) => {
       const mensajes = response.messages;
-      console.log("LISTA RECIBIDOS:", mensajes);
+
+      mensajes.array.forEach(element => {
+        this.getMensaje(element.id);
+      });
+      
       },
       (error) => this.error(error),
+    );
+  }
+
+  getMensaje(id: string){
+    this.gmail.getMessage(id).subscribe(
+      (response) => {
+        const emisor = response.payload.headers.find(e => e.name === "From");
+        const subject = response.payload.headers.find(e => e.name === "Subject");
+
+        const mensage = {
+          id: response.id,
+          cuerpo: response.snippet,
+          emisor: emisor? emisor.value : undefined,
+          titulo: subject? subject.value : undefined,
+        };
+        this.correos.push(mensage);
+      },
+      (error) => this.error(error)
     );
   }
 
